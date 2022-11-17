bf684b1faa5a9a04c89ee49776550ab801baadc7
Stefan Jansen <stefan@applied-ai.com>
2022-11-16 19:46:20 -0500

    add back sudo ubuntu

--- tools/install_talib_ubuntu.sh
+++ tools/install_talib_ubuntu.sh
@@ -4,6 +4,6 @@ curl -L -o ta-lib-0.4.0-src.tar.gz http://prdownloads.sourceforge.net/ta-lib/ta-
   tar xvfz ta-lib-0.4.0-src.tar.gz &&
   cd ta-lib &&
   ./configure &&
-  make &&
-  make install &&
-  ldconfig
+  sudo make &&
+  sudo make install &&
+  sudo ldconfig
