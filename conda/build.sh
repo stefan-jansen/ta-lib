f0af1a6c5685fc4ef13c3d6b062ed47e86ec6e8f
Stefan Jansen <stefan@applied-ai.com>
2022-11-17 13:17:00 -0500

    fiddling with build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 wget https://downloads.sourceforge.net/project/ta-lib/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz &&
+echo "Extracting ta-lib-0.4.0-src.tar.gz" &&
 tar xvfz ta-lib-0.4.0-src.tar.gz &&
+echo "Installing deps" &&
+sudo apt-get install gcc-multilib g++-multilib &&
+echo "list pwd" &&
 ls &&
 ls ta-lib &&
 pushd ta-lib &&
+echo "configure" &&
 ./configure --prefix=/usr &&
+echo "make" &&
 sudo make &&
+echo "make install" &&
 sudo make install &&
+echo "ldconfig" &&
 sudo ldconfig
 popd &&
 rm ta-lib-0.4.0-src.tar.gz &&
