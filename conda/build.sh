27383febb126fce1a3515a932fb6cde37ad2fc49
Stefan Jansen <stefan@applied-ai.com>
2022-11-17 12:05:21 -0500

    fiddling with build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 wget https://downloads.sourceforge.net/project/ta-lib/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz
 tar xvfz ta-lib-0.4.0-src.tar.gz
-echo $PREFIX
 pushd ta-lib
-./configure --prefix=$PREFIX
+./configure --prefix=/usr
 make
 make install
 popd
 rm ta-lib-0.4.0-src.tar.gz
 #rm -r ta-lib
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib
-export TA_INCLUDE_PATH=$PREFIX/include
-export TA_LIBRARY_PATH=$PREFIX/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
+export TA_INCLUDE_PATH=/usr/include
+export TA_LIBRARY_PATH=/usr/lib
 python setup.py build
-python setup.py install --prefix=$PREFIX
+python setup.py install --prefix=/usr
