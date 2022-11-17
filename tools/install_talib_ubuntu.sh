ab86504b66235a10f604c59571002e8fa90dc2b9
Stefan Jansen <stefan@applied-ai.com>
2022-11-16 19:49:52 -0500

    remove sudo ubuntu for wheel

--- tools/install_talib_ubuntu.sh
+++ tools/install_talib_ubuntu.sh
@@ -4,6 +4,6 @@ curl -L -o ta-lib-0.4.0-src.tar.gz http://prdownloads.sourceforge.net/ta-lib/ta-
   tar xvfz ta-lib-0.4.0-src.tar.gz &&
   cd ta-lib &&
   ./configure &&
-  sudo make &&
-  sudo make install &&
-  sudo ldconfig
+  make &&
+  make install &&
+  ldconfig
